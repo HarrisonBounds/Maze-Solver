8753714c9c68146887221c6582bca0caa526f43e
An Nguyen <annguyen2025@u.northwestern.edu>
2024-09-12 22:08:41 -0500

    dfs attempt

--- mazesolver.py
+++ mazesolver.py
@@ -16,12 +16,11 @@ class Square():
         return self.neighbor_list
     
 
+cur_path = []
+sln_path = []
 class MazeSolver():
     def __init__(self, maze):
         self.maze = maze
-        # self.square_list = []
-        self.cur_path = []
-        self.sln_path = []
         self.dfs_list = []
         
     def is_valid_move(self, row, col):
@@ -39,8 +38,9 @@ class MazeSolver():
 
                     if square_val == 99: # START square
                         square.isStart = True
-                        self.cur_path.append(square) 
-                        self.sln_path.append(square)
+                        square.visited = True
+                        cur_path.append(square) 
+                        sln_path.append(square)
                     
                     if square_val == 100: # GOAL square
                         square.isGoal = True
@@ -56,22 +56,27 @@ class MazeSolver():
                                 square.neighbor_list.append(neigh)
 
 
+def dfs_helper(square):
+    print("Visiting:", (square.x, square.y), square.val)
+    cur_path.append(square)
+    sln_path.append(square)
+    square.visited = True
 
-    # def display_relationships(self):
-    #     for square in self.square_list:
-    #         print(square.val,":")
-    #         for neigh in square.neighbor_list:
-    #             print(neigh.val)
+    if square.isGoal == True:
+        print("GOAL FOUND")
+        return True
 
-    def dfs(self):
-        start_row, start_col = 0, 0
-        start_squ_val = 99
-        maze_size = len(self.maze)
-        self.dfs_list.append(square_val, start_row, start_col, start_squ_val)
+    while len(square.neighbor_list) > 0:
+        neigh = square.neighbor_list.pop()
+        return dfs_helper(neigh)
 
-        for row in range (maze_size):
-            for col in range (maze_size):
-                square_val = self.maze[row][col]
+    sln_path.pop(square)
+    return False
+
+
+def dfs():
+    square = cur_path.pop()
+    return dfs_helper(square)
 
 
 example_maze = [[99, 0, 0, 1],
@@ -81,9 +86,4 @@ example_maze = [[99, 0, 0, 1],
 
 mymaze = MazeSolver(example_maze)
 mymaze.create_relationships()
-# mymaze.display_relationships()
-
-# for square in mymaze.square_list:
-#     print(square.val,": ")
-#     for neigh in square.get_neighbors():
-#         print(neigh.val)
+dfs()
