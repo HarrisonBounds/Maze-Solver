e9c47be62601f3aa79b4f9fe09adb7de6a6c6d88
An Nguyen <annguyen2025@u.northwestern.edu>
2024-09-15 13:17:22 -0500

    added git ignore

--- mazesolver.py
+++ mazesolver.py
@@ -1,3 +1,5 @@
+from var_names import *
+
 # right, down, left, up
 dirs = [(0, 1), (1, 0), (0, -1), (1, 0)] # AI Citation
 
@@ -7,10 +9,10 @@ class Square():
         self.x = x
         self.y = y
         self.isWall = isWall
-        self.visited = False
         self.isStart = False
         self.isGoal = False
         self.neighbor_list = []
+        self.visited_stack = []
 
     def get_neighbors(self):
         return self.neighbor_list
@@ -22,12 +24,21 @@ class MazeSolver():
     def __init__(self, maze):
         self.maze = maze
         self.dfs_list = []
+        self.mazegrid = [] # to access the squares and their relationships
         
     def is_valid_move(self, row, col):
         if row < len(self.maze) and row >= 0 and col < len(self.maze) and col >= 0: # within bounds
             return True
         
+    # def setup_mazegrid(self):
+    #     maze_size = len(self.maze)
+    #     for row in range (maze_size):
+    #         for col in range (maze_size):
+    #             self.mazegrid[row][col] = None
+        
     def create_relationships(self):
+        # self.setup_mazegrid()
+
         maze_size = len(self.maze)
         for row in range (maze_size):
             for col in range (maze_size):
@@ -36,13 +47,12 @@ class MazeSolver():
                 if square_val != 1: # only add neighbors for valid squares
                     square = Square(square_val, row, col, square_val == 1)
 
-                    if square_val == 99: # START square
+                    if square_val == START: # START square
                         square.isStart = True
-                        square.visited = True
                         cur_path.append(square) 
                         sln_path.append(square)
                     
-                    if square_val == 100: # GOAL square
+                    if square_val == GOAL: # GOAL square
                         square.isGoal = True
 
                     for item in dirs:
@@ -51,7 +61,7 @@ class MazeSolver():
                             neigh_val = self.maze[new_row][new_col]
                             if neigh_val != 1: # not a wall
                                 neigh = Square(neigh_val, new_row, new_col, False)
-                                if neigh_val == 100: # GOAL square
+                                if neigh_val == GOAL: # GOAL square
                                     neigh.isGoal = True
                                 square.neighbor_list.append(neigh)
 
@@ -60,17 +70,15 @@ def dfs_helper(square):
     print("Visiting:", (square.x, square.y), square.val)
     cur_path.append(square)
     sln_path.append(square)
-    square.visited = True
 
     if square.isGoal == True:
         print("GOAL FOUND")
         return True
 
-    while len(square.neighbor_list) > 0:
-        neigh = square.neighbor_list.pop()
+    for neigh in square.neighbor_list:
         return dfs_helper(neigh)
 
-    sln_path.pop(square)
+    sln_path.remove(square)
     return False
 
 
